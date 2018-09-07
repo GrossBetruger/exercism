1f01d296f54f00abff6c7031a15258113939c7ad
koi <koriatoren@gmail.com>
2018-09-07 20:01:29 +0300

    beer-song - 2 test pass

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -5,24 +5,30 @@ const VERSE_REGULAR_TAIL: &str = "Take one down and pass it around, {} bottles o
 const VERSE_ENDING_TAIL: &str = "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
 
 pub fn verse(n: i32) -> String {
+    let formatted_n = format_num(n);
+    let formatted_n_minus_one = format_num(n -1);
     match n {
-        0 => format!("{}{}",
-                     format!("{} of beer on the wall, {} of beer.\n",
-                             format_num(n), format_num(n)),
-                     "Go to the store and buy some more, 99 bottles of beer on the wall.\n"),
-        _ => format!("{}{}", format!("{} of beer on the wall, {} of beer.\n",
-                                     format_num(n), format_num(n)),
-                     format!("Take one down and pass it around, {} of beer on the wall.\n",
-                             format_num(n - 1)))
+        0 => "No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.".into(),
+        1 => format!("{} of beer on the wall, {} of beer.
+Take it down and pass it around, {} of beer on the wall.", formatted_n, formatted_n, formatted_n_minus_one),
+        _ => format!("{} of beer on the wall, {} of beer.
+Take one down and pass it around, {} of beer on the wall.", formatted_n, formatted_n, formatted_n_minus_one)
     }
 }
 
 pub fn sing(start: i32, end: i32) -> String {
     let mut song = String::new();
     for i in (end..start+1_i32).rev() {
-        song.push_str(&verse(i))
+        song.push_str(&verse(i));
+        match i == end {
+            true  =>  song.push('\n'),
+            false =>  song.push_str("\n\n"),
+        }
+//        song.push_str("\n\n");
     }
     println!("SONG:\n{}", verse(1));
+//    song.push_str("\n");
     song
 }
 
