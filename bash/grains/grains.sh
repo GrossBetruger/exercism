12b389cd1e899ed681012416e065a51d05ba92a6
koi <koriatoren@gmail.com>
2018-10-27 15:56:47 +0300

    grains: all tests pass

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+if [ "$1" = "total" ]; then 
+    echo 18446744073709551615
+    exit
+fi
+
 exp=$(($1-1))
+
+if (($exp > 63)) || (($exp < 0)); then
+    echo "Error: invalid input"
+    exit 1
+fi
 if (($exp == 63)); then 
     echo 9223372036854775808
     exit
