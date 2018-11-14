41ee3955957ac342fad8aed6e02639ec085c3622
koi <koriatoren@gmail.com>
2018-11-14 23:06:29 +0200

    haskell: clock: added lame getters

--- haskell/clock/src/Clock.hs
+++ haskell/clock/src/Clock.hs
@@ -1,14 +1,20 @@
 module Clock (addDelta, fromHourMin, toString) where
 
---data Hour = Int
---data Min = Int
-data Clock = Clock {hour :: Int, min:: Int} deriving (Show, Eq)
+
+data Clock = Clock {hour :: Int, minute:: Int} deriving (Show, Eq)
+
+getHour :: Clock -> Int
+getHour clock = hour clock
+
+getMin :: Clock -> Int
+getMin clock = minute clock
+
 
 fromHourMin :: Int -> Int -> Clock
 fromHourMin hour min = Clock hour min
 
 toString :: Clock -> String
-toString clock = error "You need to implement this function."
+toString clock = show (getHour clock) ++ ":" ++ show (getMin clock)
 
 addDelta :: Int -> Int -> Clock -> Clock
 addDelta hour min clock = error "You need to implement this function."
