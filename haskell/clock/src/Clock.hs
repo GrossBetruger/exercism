7cd4e8f9e7f3b50a925f843f7d960c54629660ab
Oren Koriat <oren@cynerio.co>
2018-11-15 18:30:56 +0200

    Clock: finally done...

--- haskell/clock/src/Clock.hs
+++ haskell/clock/src/Clock.hs
@@ -3,23 +3,20 @@ module Clock (addDelta, fromHourMin, toString) where
 
 data Clock = Clock {hour :: Int, minute:: Int} deriving (Show, Eq)
 
-
 getHour :: Clock -> Int
 getHour clock = hour clock
 
 getMin :: Clock -> Int
 getMin clock = minute clock
 
-
 overflowMins minutes = minutes `mod` 60
 overflowHours minutes = floor ((fromIntegral minutes) / 60)
 
-rawHourToClockHour :: Clock -> Int
-rawHourToClockHour clock = (((getHour clock) + (overflowHours (getMin clock))) `mod` 24)
-rawMinsToClockMins mins = (mins `mod` 60)
+rawTimeToClockHour :: Int -> Int -> Int
+rawTimeToClockHour hours mins = ((hours + (overflowHours mins)) `mod` 24)
 
 fromHourMin :: Int -> Int -> Clock
-fromHourMin hour min = Clock hour min
+fromHourMin hour min = Clock (rawTimeToClockHour hour min) (overflowMins min)
 
 showTime timeUnit
     | length (show(timeUnit)) == 1 = "0" ++ (show(timeUnit))
@@ -28,7 +25,7 @@ showTime timeUnit
 
 toString :: Clock -> String
 toString clock =
-    showTime (rawHourToClockHour clock) ++ ":" ++ showTime (rawMinsToClockMins(getMin clock))
+    showTime (getHour clock) ++ ":" ++ showTime (getMin clock)
 
 addDelta :: Int -> Int -> Clock -> Clock
-addDelta hour min clock = Clock (getHour clock + hour) (getMin clock + min)
+addDelta hour min clock = fromHourMin (getHour clock + hour) (getMin clock + min)
