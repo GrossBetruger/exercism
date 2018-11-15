8926e56b8d729ffacb94a50236628072f97a7eba
Oren Koriat <oren@cynerio.co>
2018-11-15 13:27:21 +0200

    added showTime

--- haskell/clock/src/Clock.hs
+++ haskell/clock/src/Clock.hs
@@ -3,6 +3,7 @@ module Clock (addDelta, fromHourMin, toString) where
 
 data Clock = Clock {hour :: Int, minute:: Int} deriving (Show, Eq)
 
+
 getHour :: Clock -> Int
 getHour clock = hour clock
 
@@ -13,8 +14,13 @@ getMin clock = minute clock
 fromHourMin :: Int -> Int -> Clock
 fromHourMin hour min = Clock hour min
 
+showTime timeUnit
+    | length (show(timeUnit)) == 1 = "0" ++ (show(timeUnit))
+    | otherwise = show(timeUnit)
+
+
 toString :: Clock -> String
-toString clock = show (getHour clock) ++ ":" ++ show (getMin clock)
+toString clock = showTime (abs (mod (getHour clock) 24)) ++ ":" ++ showTime (abs (mod (getMin clock) 60))
 
 addDelta :: Int -> Int -> Clock -> Clock
 addDelta hour min clock = error "You need to implement this function."
