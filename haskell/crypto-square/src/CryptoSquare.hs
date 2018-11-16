4cd1c0790aea13a7919652defa50f1d648f5bc3b
koi <koriatoren@gmail.com>
2018-11-16 14:35:33 +0200

    half tests pass

--- haskell/crypto-square/src/CryptoSquare.hs
+++ haskell/crypto-square/src/CryptoSquare.hs
@@ -1,5 +1,7 @@
 module CryptoSquare (encode) where
 
+import Data.Char
+
 edgeSize :: String -> Int
 edgeSize plain = (floor (sqrt (fromIntegral (length plain))))
 
@@ -11,6 +13,7 @@ transpose vec
   | sum (map length vec) == 0 = []
   | otherwise = [(map head vec)] ++ transpose (map tail vec)
 
+lower s = map toLower s
 
 encode :: String -> String
-encode xs = filter (\c -> not (elem c "@1,%!. \n\t")) xs
+encode xs = concat $ transpose $ (toGrid (lower (filter (\c -> not (elem c "@1,%!. \n\t")) xs)) (edgeSize xs))
