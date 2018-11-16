5b92c1d8d2c1d6566418ddd21f01a0e50c3c0295
koi <koriatoren@gmail.com>
2018-11-16 14:45:17 +0200

    haskell: cryptosqaure - done

--- haskell/crypto-square/src/CryptoSquare.hs
+++ haskell/crypto-square/src/CryptoSquare.hs
@@ -1,6 +1,7 @@
 module CryptoSquare (encode) where
 
 import Data.Char
+import Data.List
 
 edgeSize :: String -> Int
 edgeSize plain = (floor (sqrt (fromIntegral (length plain))))
@@ -9,11 +10,15 @@ toGrid xs dimension
   | xs == [] = []
   | otherwise = [take dimension xs] ++ (toGrid (drop dimension xs) dimension)
 
-transpose vec
+transposeGrid vec
   | sum (map length vec) == 0 = []
   | otherwise = [(map head vec)] ++ transpose (map tail vec)
 
 lower s = map toLower s
 
+joinSpaced s = intercalate " " s
+
+clean s = filter (\c -> not (elem c "@,%!. \n\t")) s
+
 encode :: String -> String
-encode xs = concat $ transpose $ (toGrid (lower (filter (\c -> not (elem c "@1,%!. \n\t")) xs)) (edgeSize xs))
+encode xs = joinSpaced $ transposeGrid $ (toGrid (lower $ clean xs) (edgeSize xs))
