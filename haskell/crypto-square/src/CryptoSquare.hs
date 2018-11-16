de3ad00f0deca376d76e2508adea3cf22f4ca15e
koi <koriatoren@gmail.com>
2018-11-16 14:30:35 +0200

    added transpose method

--- haskell/crypto-square/src/CryptoSquare.hs
+++ haskell/crypto-square/src/CryptoSquare.hs
@@ -7,5 +7,10 @@ toGrid xs dimension
   | xs == [] = []
   | otherwise = [take dimension xs] ++ (toGrid (drop dimension xs) dimension)
 
+transpose vec
+  | sum (map length vec) == 0 = []
+  | otherwise = [(map head vec)] ++ transpose (map tail vec)
+
+
 encode :: String -> String
 encode xs = filter (\c -> not (elem c "@1,%!. \n\t")) xs
