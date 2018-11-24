bcd35dd26e1c7d1476ec568535a0d2a6d9ebf33c
koi <koriatoren@gmail.com>
2018-11-24 15:18:57 +0200

    rust: VLQ - group byte stream into chunks of msbs and lsb

--- rust/variable-length-quantity/src/lib.rs
+++ rust/variable-length-quantity/src/lib.rs
@@ -39,6 +39,26 @@ pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
     if bytes.len() == 1 {
         return Ok(vec![*bytes.get(0).unwrap() as u32]);
     }
+
+//    let mut single_num_bytes = vec![];
+    let results = vec![1];
+    let groups = bytes.iter()
+        .group_by(|b| *b & 128 == 128)
+        .into_iter().map(|(pred, group)| group.cloned().collect()).collect::<Vec<Vec<u8>>>();
+
+    let nums_iter = groups.iter().chunks(2);
+
+    for num in nums_iter.into_iter() {
+        let num_parts = num.collect_vec();
+        let msbs: Vec<u8> = num_parts.get(0).unwrap().to_vec();
+        let lsb: u8 = *num_parts.get(1).unwrap().get(0).unwrap();
+        println!("msbs: {:?} lsb: {:?}", msbs, lsb);
+    }
+
+    Ok(vec![1])
+}
+
+fn parse_single_number(bytes: &[u8]) {
     let mut stream = String::new();
     for byte in bytes.iter().take_while(|b| *b & 128 == 128) {
         stream.push_str(&skip_msb(&byte_to_bin(*byte)))
@@ -46,8 +66,6 @@ pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
     let least_significant_byte = &skip_msb(&byte_to_bin(*bytes.get(bytes.len()-1).unwrap()));
     stream.push_str(least_significant_byte);
     let result = bin_to_num64(&stream.chars().collect()) as u32;
-    println!("binary: '{}', num: '{}', lsbyte: '{}'", stream,  result, least_significant_byte);
-    Ok(vec![result])
 }
 
 fn byte_to_bin(num: u8) -> String {
