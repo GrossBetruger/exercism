a14ab948e3ce55b8a0f8dba4fcaf99e770d78889
koi <koriatoren@gmail.com>
2018-11-24 18:15:42 +0200

    rust: VLQ - cleanup

--- rust/variable-length-quantity/src/lib.rs
+++ rust/variable-length-quantity/src/lib.rs
@@ -1,4 +1,4 @@
-#[macro_use] extern crate itertools;
+extern crate itertools;
 
 use itertools::Itertools;
 
@@ -22,20 +22,15 @@ pub fn to_bytes(values: &[u32]) -> Vec<u8> {
                     sevens.push(seven.collect::<String>());
                 }
 
-//                let mut bytes = vec![];
-
                 for (lsb, msbs) in sevens.split_first() {
-                    println!("msbs {:?} lsb {:?}", msbs, lsb);
                     for msb in msbs.iter().rev() {
                         let msb = msb.chars().rev().collect::<String>();
                         let msb_str: Vec<char> = pad_byte(&msb, "1").chars().collect();
                         res.push(bin_to_byte(&msb_str));
-                        println!("msb {:?}", msb_str);
-                        println!("results: {:?}", &res);
+
                     }
                     let lsb = lsb.chars().rev().collect::<String>();
                     res.push(bin_to_byte(&pad_byte(&lsb, "0").chars().collect()));
-                    println!("results: {:?}", &res);
 
                 }
             }
@@ -54,11 +49,10 @@ pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
         return Ok(vec![*bytes.get(0).unwrap() as u32]);
     }
 
-//    let mut single_num_bytes = vec![];
     let mut results = vec![];
     let groups = bytes.iter()
         .group_by(|b| *b & 128 == 128)
-        .into_iter().map(|(pred, group)| group.cloned().collect()).collect::<Vec<Vec<u8>>>();
+        .into_iter().map(|(_pred, group)| group.cloned().collect()).collect::<Vec<Vec<u8>>>();
 
     let nums_iter = groups.iter().chunks(2);
 
@@ -66,7 +60,7 @@ pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
         let num_parts = num.collect_vec();
         let msbs: Vec<u8> = num_parts.get(0).unwrap().to_vec();
         let number_of_lsbs = num_parts.get(1).unwrap().len();
-//        println!("lsb size: {:?}", num_parts.get(1).unwrap().len());
+
         match number_of_lsbs {
             1 => {
                 let lsb: u8 = *num_parts.get(1).unwrap().get(0).unwrap();
@@ -74,8 +68,7 @@ pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
                 if calculated_num  > u32::max_value() as u64 {
                     return Err(Error::Overflow);
                 }
-//                println!("msbs: {:?} lsb: {:?}", msbs, lsb);
-//                println!("number: {}", &calculated_num);
+
                 results.push(calculated_num as u32);
             }
             _ => {
@@ -84,8 +77,7 @@ pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
                 if calculated_num  > u32::max_value() as u64 {
                     return Err(Error::Overflow);
                 }
-//                println!("msbs: {:?} lsb: {:?}", msbs, lsb);
-//                println!("number: {}", &calculated_num);
+
                 results.push(calculated_num as u32);
 
                 for lsb in num_parts.get(1).unwrap().iter().skip(1) {
@@ -127,10 +119,6 @@ fn skip_msb(byte_repr: &str) -> String {
     byte_repr.chars().skip(1).collect()
 }
 
-fn strip_msb(byte: u8) -> u8 {
-    byte & 127
-}
-
 fn pad_byte(byte_repr: &str, first: &str) -> String {
     let padding = String::from("0").repeat(7 - byte_repr.len());
     format!("{}{}{}", first, padding, byte_repr)
@@ -143,22 +131,8 @@ fn bin_to_byte(bin: &Vec<char>) -> u8 {
     })
 }
 
-
-fn bin_to_num32(bin: &Vec<char>) -> u32 {
-    bin.iter().rev().enumerate().fold(0_u32, {
-        |acc: u32, (e, bin_dig)| acc + bin_dig.to_digit(2).unwrap() as u32 * 2_u32.pow(e as u32)
-    })
-}
-
 fn bin_to_num64(bin: &Vec<char>) -> u64 {
     bin.iter().rev().enumerate().fold(0_u64, {
         |acc: u64, (e, bin_dig)| acc + bin_dig.to_digit(2).unwrap() as u64 * 2_u64.pow(e as u32)
     })
-}
-
-fn overflow7(num: u8) -> u8 {
-    match num > 128 {
-        true => 128 + 1,
-        _ => num
-    }
 }
