0f1f5eead3be7cb2049f28ff137509eb1eb1b5bd
koi <koriatoren@gmail.com>
2018-11-24 19:31:24 +0200

    rust: piglatin - almost all tests pass

--- rust/pig-latin/src/lib.rs
+++ rust/pig-latin/src/lib.rs
@@ -2,16 +2,33 @@
 
 const VOWELS: [char; 5] = ['a', 'e', 'u', 'i', 'o'];
 
+const TRIPLE_CONSONANTS: [&str; 2] = ["thr", "sch"];
+
+const DOUBLE_CONSONANTS: [&str; 3] = ["th", "sh", "ch"];
+
+
 pub fn translate(input: &str) -> String {
     match first_is_vowel(&input) {
         true => return vowel_ay(&input),
         false => {
-            match has_qu(&input) {
+            match has_consonant_qu(&input) {
                 true => {
                     println!("hasqu alright!!");
                     return consonant_qu_swap(&input)
                 },
-                false => return consonant_swap(&input)
+                false => {
+                    match has_qu(input) {
+                        true => {
+                            println!("simple qu");
+                            return qu_swap(input)
+                        },
+                        false => {
+                            println!("time for consonant swap");
+                            return consonant_swap(&input)
+                        }
+                    };
+
+                }
             }
         }
     }
@@ -32,6 +49,11 @@ fn consonant_qu_swap(input: &str) -> String {
 
 }
 
+fn qu_swap(input: &str) -> String {
+    format!("{}quay", input.chars().skip(2).collect::<String>())
+
+}
+
 fn first_is_vowel(input: &str) -> bool {
     input.chars().take(1).any(|x| VOWELS.contains(&x))
         || ["yt".into(), "xr".into()]
@@ -39,23 +61,38 @@ fn first_is_vowel(input: &str) -> bool {
 }
 
 fn get_first_unsafe(input: &str) -> String {
-    match ["th".into(), "sh".into(), "ch".into()]
-        .contains(&input.chars().take(2).collect::<String>()) {
-        false => input.chars().take(1).collect::<String>(),
-        true => input.chars().take(2).collect::<String>()
-
+    match TRIPLE_CONSONANTS
+        .contains(&input.chars().take(3).collect::<String>().as_str()) {
+            false => {
+                match DOUBLE_CONSONANTS
+                .contains(&input.chars().take(2).collect::<String>().as_str()) {
+                false => input.chars().take(1).collect::<String>(),
+                true => input.chars().take(2).collect::<String>()
+                }
+            },
+            true => input.chars().take(3).collect::<String>()
     }
 }
 
 fn skip_first(input: &str) -> String {
-    match ["th".into(), "sh".into(), "ch".into()]
-        .contains(&input.chars().take(2).collect::<String>()) {
-        false => input.chars().skip(1).collect::<String>(),
-        true => input.chars().skip(2).collect::<String>()
-
+    match TRIPLE_CONSONANTS
+        .contains(&input.chars().take(3).collect::<String>().as_str()) {
+        false =>    {
+            match DOUBLE_CONSONANTS
+                .contains(&input.chars().take(2).collect::<String>().as_str()) {
+                false => input.chars().skip(1).collect::<String>(),
+                true => input.chars().skip(2).collect::<String>()
+                }
+            }
+        true => input.chars().skip(3).collect::<String>()
     }
 }
 
+
+fn has_consonant_qu(input: &str) -> bool {
+    &skip_first(input).chars().take(2).collect::<String>() == "qu"
+}
+
 fn has_qu(input: &str) -> bool {
-    &skip_first(input).chars().skip(1).take(2).collect::<String>() == "qu"
+    &input.chars().take(2).collect::<String>() == "qu"
 }
