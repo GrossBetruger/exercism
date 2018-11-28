a2f1ee0679c830d9cc97cd121af036b390dba3b1
koi <koriatoren@gmail.com>
2018-11-28 20:23:06 +0200

    haskell: robot-simulator -> all tests pass

--- haskell/robot-simulator/src/Robot.hs
+++ haskell/robot-simulator/src/Robot.hs
@@ -23,7 +23,22 @@ coordinates robot = coords robot
 mkRobot :: Bearing -> (Integer, Integer) -> Robot
 mkRobot direction coordinates = Robot direction coordinates
 
+addToX (x, y) n = ((x + n), y)
+
+addToY (x, y) n = (x, (y + n))
+
 move :: Robot -> String -> Robot
 move robot instructions
-  | instructions == "turn right" && bearing robot == East = Robot West (coordinates robot)
-  | otherwise = robot
+  | length instructions == 0 = robot
+  | head instructions == 'R' && bearing robot == East = move (Robot South (coordinates robot)) (tail instructions)
+  | head instructions == 'R' && bearing robot == South = move (Robot West (coordinates robot))  (tail instructions)
+  | head instructions == 'R' && bearing robot == West = move (Robot North (coordinates robot))  (tail instructions)
+  | head instructions == 'R' && bearing robot == North = move (Robot East (coordinates robot))  (tail instructions)
+  | head instructions == 'L' && bearing robot == East = move (Robot North (coordinates robot))  (tail instructions)
+  | head instructions == 'L' && bearing robot == South = move (Robot East (coordinates robot))  (tail instructions)
+  | head instructions == 'L' && bearing robot == West = move (Robot South (coordinates robot))  (tail instructions)
+  | head instructions == 'L' && bearing robot == North = move (Robot West (coordinates robot))  (tail instructions)
+  | head instructions == 'A' && bearing robot == East = move (Robot East (addToX (coordinates robot) 1))  (tail instructions)
+  | head instructions == 'A' && bearing robot == South = move (Robot South (addToY (coordinates robot) (-1)))  (tail instructions)
+  | head instructions == 'A' && bearing robot == West = move (Robot West (addToX (coordinates robot) (-1)))  (tail instructions)
+  | head instructions == 'A' && bearing robot == North = move (Robot North (addToY (coordinates robot) 1))  (tail instructions)
