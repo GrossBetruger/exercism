7d95626e8f0fa8aea59b27bd82c91cf62019adbe
Oren Koriat <oren@cynerio.co>
2018-11-28 11:25:07 +0200

    haskell: bob cr comments

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,25 +1,25 @@
 module Bob (responseFor) where
 
 import Data.Char
+import Data.List
 
 responseFor :: String -> String
 responseFor xs
-  | isQuestYell xs = "Calm down, I know what I'm doing!"
+  | any isSpace xs = responseFor $ clearWhitespace xs
+  | null xs = "Fine. Be that way!"
+  | isQuestionYell xs = "Calm down, I know what I'm doing!"
   | isQuestion xs = "Sure."
   | isYell xs = "Whoa, chill out!"
-  | (clearWhitespace xs) == "" = "Fine. Be that way!"
   | otherwise = "Whatever."
 
 filterNonAlpha sentence = filter (\c -> isAlpha c) sentence
 
-clearWhitespace sentence = filter (\c -> not (elem c " \n\t\r")) sentence
+clearWhitespace sentence = filter (not . isSpace) sentence
 
 isYell sentence
-  | length (filterNonAlpha sentence) == 0 = False
+  | all (not . isAlpha) sentence = False
   | otherwise = all (\c -> isUpper c) (filterNonAlpha sentence)
 
-isQuestion sentence
-  | length (clearWhitespace sentence) == 0 = False
-  | otherwise = last (clearWhitespace sentence) == '?'
+isQuestion sentence = isSuffixOf "?" sentence
 
-isQuestYell sentence = isYell sentence && isQuestion sentence
+isQuestionYell sentence = isYell sentence && isQuestion sentence
