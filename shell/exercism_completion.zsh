ca9ee2a399d706847227564181b37e4acda4028a
koi <koriatoren@gmail.com>
2018-12-18 22:40:41 +0200

    updated exercism

--- shell/exercism_completion.zsh
+++ shell/exercism_completion.zsh
@@ -3,22 +3,20 @@ _exercism() {
     typeset -A opt_args
 
     local -a options
-    options=(debug:"Outputs useful debug information."
-             configure:"Writes config values to a JSON file."
-             demo:"Fetches a demo problem for each language track on exercism.io."
-             fetch:"Fetches your current problems on exercism.ios well as the next unstarted problem in each language."
-             restore:"Restores completed and current problems on from exercism.iolong with your most recent iteration for each."
-             submit:"Submits a new iteration to a problem on exercism.io."
-             unsubmit:"Deletes the most recently submitted iteration."
-             tracks:"List the available language tracks"
+    options=(configure:"Writes config values to a JSON file."
              download:"Downloads and saves a specified submission into the local system"
+             open:"Opens a browser to exercism.io for the specified submission."
+             submit:"Submits a new iteration to a problem on exercism.io."
+             troubleshoot:"Outputs useful debug information."
+             upgrade:"Upgrades to the latest available version."
+             version:"Outputs version information."
+             workspace:"Outputs the root directory for Exercism exercises."
              help:"Shows a list of commands or help for one command")
 
     _arguments -s -S \
-        {-c,--config}"[path to config file]:file:_files"    \
-        {-d,--debug}"[turn on verbose logging]" \
         {-h,--help}"[show help]"                \
-        {-v,--version}"[print the version]"     \
+        {-t,--timeout}"[override default HTTP timeout]" \
+        {-v,--verbose}"[turn on verbose logging]" \
         '(-): :->command'                       \
         '(-)*:: :->option-or-argument'          \
         && return 0;
